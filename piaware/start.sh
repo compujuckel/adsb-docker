e779d2e5de125eebb1ce618c544e0af2fae5e07f
compujuckel <github@mackeben.eu>
2019-02-11 20:21:05 +0100

    add flightaware feeder id

--- piaware/start.sh
+++ piaware/start.sh
@@ -6,4 +6,8 @@
 /usr/bin/piaware-config receiver-host "${RECEIVER_HOST}"
 /usr/bin/piaware-config receiver-port "${RECEIVER_PORT}"
 
+if [ -n "${FLIGHTAWARE_FEEDER_ID}" ]; then
+  /usr/bin/piaware-config feeder-id "${FLIGHTAWARE_FEEDER_ID}"
+fi
+
 supervisord -c /etc/supervisor/supervisord.conf
