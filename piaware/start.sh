63d3a029978bf5c2c3f94494937c3821704b9ea4
compujuckel <github@mackeben.eu>
2019-02-11 21:06:01 +0100

    fix flightaware MLAT

--- piaware/start.sh
+++ piaware/start.sh
@@ -2,6 +2,7 @@
 
 /usr/bin/piaware-config flightaware-user "${FLIGHTAWARE_USER}"
 /usr/bin/piaware-config flightaware-password "${FLIGHTAWARE_PASSWORD}"
+/usr/bin/piaware-config mlat-results-format "beast,connect,${RECEIVER_HOST}:${RECEIVER_MLAT_PORT} beast,listen,30105 ext_basestation,listen,30106"
 /usr/bin/piaware-config receiver-type other
 /usr/bin/piaware-config receiver-host "${RECEIVER_HOST}"
 /usr/bin/piaware-config receiver-port "${RECEIVER_PORT}"
